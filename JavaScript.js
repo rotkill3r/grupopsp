e4f78a3099488a45ed919e1b5739ebb3f816e431
rotkill3r <43934897+rotkill3r@users.noreply.github.com>
2021-05-29 12:26:51 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,40 @@
-
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Gayathri&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/7045981063.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <div class="logo">
+            <div class="header-logo"></div>
+            <h1 class="kind"> <i class="fas "></i> PoliGeek </h1>
+        </div>
+        <nav class="nav-header">
+            <a href="index.html">Home</a>
+            <a href="#">News</a>            
+            <a href="#">Acerca de</a>
+        </nav>
+    </header>
+    <main>
+        <div class="left">
+            <div class="opciones">                
+                <div><a href="#" target="contenido"> #</a></div>
+                <div><a href="#" target="contenido"> # </a></div>
+                <div><a href="#" target="contenido"> # </a></div>                
+            </div>  
+            <hr>
+            <div class="Clasificaciones">
+                
+            </div>
+        </div>
+        <div class="right"> 
+            <iframe src="#" name="contenido" frameborder="0" width="100%" height="100%"></iframe>        
+        </div>
+    </main>
+</body>
+</html>
